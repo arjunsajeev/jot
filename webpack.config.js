05a6c357b32d7489b6084c7ac164b79d6637b3b1
Arjun Sajeev <arjunsajeev04@gmail.com>
2018-12-15 17:03:15 +0530

    Add clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/js/app.js',
@@ -7,13 +8,32 @@ module.exports = {
     path: __dirname + '/dist',
     filename: 'bundle.js'
   },
+  mode: 'production',
+  devServer: {
+    contentBase: './dist'
+  },
   module: {
-    rules: [{
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }]
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
+    new CleanWebpackPlugin('dist'),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
